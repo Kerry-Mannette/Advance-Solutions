7789d8f262587869f838416506ab1e745a0157a7
Kerry Mannette <kerrymannette2@gmail.com>
2025-10-21 19:16:17 -0400

    active update

---
+++ js/active.js
@@ -0,0 +1,10 @@
+let navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    navbarLinks.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+
+
